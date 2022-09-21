eb17fe1e8e7f1c782478c20dcc2c3aae789fda34
natalya zhelezko <kyraginaa@mail.ru>
2022-09-21 20:55:54 +0300

    add solution for task 4

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿//=================================================================
+// # 4 Напишите программу, которая принимает на вход три числа и 
+// выдаёт максимальное из этих чисел.
+//=================================================================
+
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+string? inputLineC = Console.ReadLine();
+
+if (inputLineA != null && inputLineB != null && inputLineC != null)
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+    int inputNumberC = int.Parse(inputLineC);
+
+    if (inputNumberA > inputNumberB && inputNumberA > inputNumberC)
+        {
+            Console.Write("max = ");
+            Console.Write(inputNumberA);
+        }
+    else
+    if (inputNumberB > inputNumberA && inputNumberB > inputNumberC)
+        {
+            Console.Write("max = ");
+            Console.Write(inputNumberB);
+        }
+    else
+    if (inputNumberC > inputNumberA && inputNumberC > inputNumberB)
+        {
+            Console.Write("max = ");
+            Console.Write(inputNumberC);
+        }
+}
