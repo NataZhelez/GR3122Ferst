7ffa48324064ff56c5f8aa016ca758fd9b4628b1
natalya zhelezko <kyraginaa@mail.ru>
2022-09-21 18:26:54 +0300

    add solution for task 2

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿//=================================================================
+// # 2  Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+//=================================================================
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+
+if (inputLineA != null && inputLineB != null)
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+
+    if (inputNumberA > inputNumberB)
+    {
+        Console.WriteLine(inputNumberA);
+    }
+    else
+    if (inputNumberB > inputNumberA)
+    {
+        Console.WriteLine(inputLineB);
+    }
+}
