95b4e8dec7745197beb838ab7cdb09ea76761083
natalya zhelezko <kyraginaa@mail.ru>
2022-09-22 22:47:56 +0300

    Добавить решение задачи 6, 8

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,21 @@
+﻿//=================================================================
+// # 6 Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+//=================================================================
+string? inputLineA = Console.ReadLine();
+
+if (inputLineA != null)
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberR = inputNumberA % 2;
+
+    if (inputNumberR == 0)
+    {
+        Console.WriteLine("чётное");
+    }
+    else
+    if (inputNumberR == 1)
+    {
+        Console.WriteLine("нечётное");
+    }
+}
---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,34 @@
+﻿//=================================================================
+// # 8 Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+//=================================================================
+
+string? inputLineN = Console.ReadLine();
+// на вход принимаем число N
+
+if (inputLineN != null)
+{
+    int inputNumberN = int.Parse(inputLineN);
+    // Превращаем в целое число
+
+    int startNumber = 1;
+    //Вводим дополнительную переменную 
+
+    while (startNumber <= inputNumberN)
+    // Теперь цикл
+    {
+        if (startNumber % 2 == 0)
+        {
+            Console.Write(startNumber + ", ");
+            startNumber = startNumber + 1;
+            // Между числами расстояние и пробел
+        }
+        else
+        {
+            startNumber = startNumber + 1;
+            // 
+        }
+
+    }
+}
+
