45309de750f07c8a6f87e904e3ba8e1c3dfc958b
natalya zhelezko <kyraginaa@mail.ru>
2022-09-27 07:51:07 +0300

    Добавили решение задач 9, 10, 11, 12, 13, 14, 15

---
+++ Sem2Task12/Program.cs
@@ -0,0 +1,88 @@
+﻿//=================================================================
+// #12  Напишите программу, которая  на вход принимает два числа и выводит
+// является ли второе число кратным первому, если второе 
+// число некратно первому, то программа выводит остаток от деления. 
+//=================================================================
+
+int inputNumberA = 0;
+int inputNumberB = 0;
+bool result = false;
+
+ReadData();
+ConculateData();
+PrintData();
+
+
+// Получаем два числа от пользователя
+void ReadData()
+{
+    Console.Write("Введите первое число: ");
+    string? inputLineA = Console.ReadLine();
+    Console.Write("Введите второе число: ");
+    string? inputLineB = Console.ReadLine();
+
+    if (inputLineA != null && inputLineB != null)
+    {
+        inputNumberA = int.Parse(inputLineA);
+        inputNumberB = int.Parse(inputLineB);
+    }
+
+}
+
+//Определяем кратность чисел
+void ConculateData()
+{
+    result = (inputNumberB % inputNumberA == 0);
+}
+
+//Выводим данные вычисления
+void PrintData()
+{
+    if (result)
+    {
+        Console.WriteLine("Второе число кратно первому");
+    }
+    else
+    {
+        Console.WriteLine("Остаток от деления: " + inputNumberB % inputNumberA);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//string? inputLineA = Console.ReadLine();
+//string? inputLineB = Console.ReadLine();
+
+
+//if (inputLineA != null && inputLineB != null)
+//{
+//  int inputNumberA = int.Parse(inputLineA);
+//int inputNumberB = int.Parse(inputLineB);
+
+// Превращаем в целое число
+// Получение данных
+//bool inputNumberR = (inputNumberB % inputNumberA == 0);
+//Вычисление
+//if (inputNumberR)
+// {
+//   Console.WriteLine("inputNumberB кратно inputNumberA");
+//}
+//else
+//{
+//  Console.WriteLine("не кратно, " + (inputNumberB % inputNumberA));
+//}
+//Вывод результата
+//}
+
+
+
---
+++ Sem2Task9/Program.cs
@@ -0,0 +1,51 @@
+﻿//=================================================================
+// # 9 Напишите программу, которая выводит случайное число из отрезка [10,99]
+// и показывает наибольшую цифру числа. 
+//=================================================================
+
+void MyVariant()
+{
+    Console.WriteLine("Метод 1");
+    System.Random numberGenerator = new System.Random();
+    // создали переменную и в оперативной памяти выбрали ей место
+
+    int number = numberGenerator.Next(10, 100);
+    // теперь создаём число
+
+    Console.WriteLine(number);
+
+    int ferstDigit = number / 10;
+    int secondDigit = number % 10;
+
+    // ввели еще переменные
+
+    if (ferstDigit > secondDigit)
+    {
+        Console.WriteLine(ferstDigit);
+    }
+    else
+    {
+        Console.WriteLine(secondDigit);
+    }
+}
+
+void CharVariant()
+
+{
+    // Вариант char
+    Console.WriteLine("Метод 2");
+    System.Random numberGenerator = new System.Random();
+    // создали переменную и в оперативной памяти выбрали ей место
+
+    int number = numberGenerator.Next(10, 100);
+    // теперь создаём число
+
+    Console.WriteLine(number);
+    char[] charArray = number.ToString().ToCharArray();
+    Console.WriteLine(((int)charArray[0] > (int)charArray[1]) ? charArray[0] : charArray[1]);
+}
+
+
+MyVariant();
+
+CharVariant();
