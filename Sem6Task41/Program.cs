2208082e0472ca6ef9d8a29ff045fdb21a95f469
natalya zhelezko <kyraginaa@mail.ru>
2022-10-07 19:14:14 +0300

    Добавили задачи 41, 43

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,35 @@
+﻿//==========================================================
+// # 41 Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//=========================================================
+double ReadData(string line)
+{   
+    Console.Write(line);   
+    //Считываем число и преобразуем из строки в double
+    double x1= double.Parse(Console.ReadLine() ?? "0"); 
+    //Возвращаем значение
+    return x1;  
+}
+//Анализ чисел на положительность и подсчет тех, что >0
+int Count(double num)
+{
+    int res = 0;
+    for (int i = 0; i < num; i++)
+    {
+        double number = ReadData("");
+        if (number>0)
+        {
+            res++;
+        }        
+    }
+    return res;
+}
+//Вывод результата
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+double countNumber = ReadData("Введите планируемое для анализа на положительность количество чисел ");
+Console.WriteLine("Вводите числа по одному и нажимайте Enter");
+PrintResult("Количество введенных положительных чисел = " + Count(countNumber));
