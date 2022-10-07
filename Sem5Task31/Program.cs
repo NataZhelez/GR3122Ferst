9f5904ad336c7c85122e68248771cb073e1b2cec
natalya zhelezko <kyraginaa@mail.ru>
2022-10-07 18:58:13 +0300

    Добавили задачи 31, 32, 33, 34, 35, 36, 37, 38

---
+++ Sem5Task31/Program.cs
@@ -0,0 +1,79 @@
+﻿//====================================================
+// # 31 Задайте массив из 12 элементов, зполненный
+// случайными числами из промежутка [-9,9]. Найдите сумму
+// отрицательных и положительных элементов массива.
+//====================================================
+
+int ReadData(string line)
+{
+    // Выводим сообщение 
+    Console.WriteLine(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+// Универсальный метод генерации и заполнения массива
+int[] FillArray(int num, int downBorder, int topBorder)
+{
+    //Генератор случайных чисел
+    Random numSintesator = new Random();
+    // Создаём массив
+    int[] arr = new int[num];
+
+    if (downBorder < topBorder)
+    {
+
+        // Заполняем массив
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            arr[i] = numSintesator.Next(downBorder, topBorder + 1);
+        }
+    }
+    //Возвращаем рузультат
+    return arr;
+}
+//Печатаем одномерный массив
+void PrintArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1]);
+}
+
+int[] NegotivPosittivSums(int[] arr)
+{
+    int[] sums = new int[2];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            sums[0] += arr[1];
+        }
+        else
+        {
+            sums[1] += arr[i];
+        }
+    }
+    return sums;
+}
+
+// Метод вывода результата
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+
+int arrayLength = ReadData("Введите длину массива: ");
+int downBorder = ReadData("Введите нижнюю границу заполнения массива: ");
+int topBorder = ReadData("Введите верхнюю границу заполнения массива: ");
+int[] imputArray = FillArray(arrayLength, downBorder, topBorder);
+PrintArr(imputArray);
+int[] sumArr = NegotivPosittivSums(imputArray);
+PrintResult("Сумма > 0:  " + sumArr[0] +  "Сумма < 0: " + sumArr[1]);
+PrintArr(sumArr);
+
+
---
+++ Sem5Task34/Program.cs
@@ -0,0 +1,70 @@
+﻿//==================================================
+//# 34 Задайте массив заполненный случайными 
+// положительными трёхзначными числами.
+// Напишите программу, которая покажет количество 
+// чётных чисел в массиве.
+//=================================================
+// Генерация случайного массива.
+int[] GenArr(int arrLen, int arrMin, int arrMax)
+{
+    int[] arr = new int[arrLen];
+    Random rnd = new Random();
+
+    if (arrMin > arrMax)
+    {
+        int temp = arrMax;
+        arrMax = arrMin;
+        arrMin = temp;
+    }
+
+    for (int i = 0; i < arrLen; i++)
+    {
+        arr[i] = rnd.Next(arrMin, arrMax);
+    }
+    return arr;
+}
+
+// Печать массива
+void PrintArr(int[] arr, string message = "Массив: ")
+{
+    Console.WriteLine($"{message}[{string.Join(", ", arr)}]");
+}
+
+// Сортировка пузырьком.
+int[] BubbleSort(int[] arr)
+{
+    for (int i = 1; i < arr.Length; i++)
+    {
+        for (int j = 0; j < arr.Length - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+
+// Счетчик четных чисел.
+int EvenCount(int[] arr)
+{
+    int count = 0;
+    foreach (int item in arr)
+    {
+        if (item % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+
+int[] arr = GenArr(10, 100, 1000);
+PrintArr(arr);
+int[] sortArr = BubbleSort(arr);
+PrintArr(sortArr, "Сортированный массив: ");
+Console.WriteLine($"Количество четных элементов: {EvenCount(arr)}");
